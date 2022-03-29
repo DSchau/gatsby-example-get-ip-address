0512f8f249978e30101de62fd415a73b7b25e775
Dustin Schau <dustinschau@gmail.com>
2022-03-29 14:18:57 -0500

    chore: update

--- src/api/ip-address.js
+++ src/api/ip-address.js
@@ -1,5 +1,5 @@
 export default async function getIPAddress(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
   return res.status(200).json({
     ip
