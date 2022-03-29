b740559669f857151b68d6733a65d9bae8f33e29
Dustin Schau <dustinschau@gmail.com>
2022-03-29 14:24:01 -0500

    chore: update main

--- src/api/ip-address.js
+++ src/api/ip-address.js
@@ -1,7 +1,12 @@
+/*
+ * x-forwarded-for is for reverse proxy
+ * connection.remoteAddress is typical path
+ */
 export default async function getIPAddress(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
   return res.status(200).json({
-    ip
+    ip,
+    options: [req.ip, req.headers['x-forwarded-for'], req.connection.remoteAddress]
   })
 }
